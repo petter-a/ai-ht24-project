6c8861643fd1ce7c44d65810108bf471d83b909a
Petter Andersson <petter.andersson@helsingborg.se>
2024-11-25 15:27:06 +0100

    initial

--- src/plot.py
+++ src/plot.py
@@ -15,6 +15,7 @@ class Plot:
 
         self.axs[0].xaxis.set_minor_locator(mdates.DayLocator())
         self.axs[0].xaxis.set_major_formatter(mdates.ConciseDateFormatter(self.axs[0].xaxis.get_major_locator()))
+        self.axs[0].yaxis.set_major_formatter('${x:1.2f}')
 
     def format_title(self):
         return f'Date: {self.stock.get_closing_date().strftime('%Y-%m-%d')}\nClosing price: ${self.stock.get_closing_price():1.2f}'
@@ -35,12 +36,12 @@ class Plot:
 
     def plot_stock_price(self):
         prices = self.stock.get_price(self.range)
-        self.axs[0].plot(prices, color='green', label='Price')
+        self.axs[0].plot(prices, color='green', label='Price', linewidth=3)
 
     def plot_stock_prediction(self):
         prices = self.stock.get_predicted_price()
         self.axs[0].axvline(prices.index[0])
-        self.axs[0].plot(prices['Close'], color='green', label='Predicted Price', linestyle=(0,(2,2)))
+        self.axs[0].plot(prices['Close'], color='green', label='Predicted Price', linestyle=(0,(2,2)), linewidth=3)
         (bottom, top) = self.axs[0].get_ylim()
         self.axs[0].text(prices.index[0], top - ((top - bottom)/2), 'Future', rotation=90, verticalalignment='center')
 #        self.axs[0].plot(prices['SMA_high'], color='red', label='SMA 200', linestyle="dotted")
@@ -52,24 +53,24 @@ class Plot:
 
     def plot_max_price(self):
         prices = self.stock.get_price(self.range)
-        self.axs[0].axhline(y=prices.max(), color="grey", linestyle="--", label=f'Max: ${prices.max():1.2f}')
+        self.axs[0].axhline(y=prices.max(), color="grey", linestyle="--", label=f'Max: ${prices.max():1.2f}', alpha=0.2)
         #self.ax.plot(prices.filter([prices.idxmax()]), marker='*')
 
     def plot_min_price(self):
         prices = self.stock.get_price(self.range)
-        self.axs[0].axhline(y=prices.min(), color="grey", linestyle="--", label=f'Min: ${prices.min():1.2f}')
+        self.axs[0].axhline(y=prices.min(), color="grey", linestyle="--", label=f'Min: ${prices.min():1.2f}', alpha=0.2)
 
     def plot_sma200(self):
         prices = self.stock.get_sma_high(self.range)
-        self.axs[0].plot(prices, color='red', label='SMA200')
+        self.axs[0].plot(prices, color='red', label='SMA200', alpha=0.3)
         
     def plot_sma50(self):
         prices = self.stock.get_sma_low(self.range)
-        self.axs[0].plot(prices, color='blue', label='SMA50')
+        self.axs[0].plot(prices, color='blue', label='SMA50', alpha=0.3)
 
     def plot_mean_price(self):
         prices = self.stock.get_price(self.range)
-        self.axs[0].axhline(y=prices.mean(), color='brown', linestyle="--", label=f'Medel: ${prices.mean():1.2f}')
+        self.axs[0].axhline(y=prices.mean(), color='brown', linestyle="--", label=f'Medel: ${prices.mean():1.2f}', alpha=0.2)
 
     def plot_rsi(self):
         prices = self.stock.get_rsi_val(self.range)
